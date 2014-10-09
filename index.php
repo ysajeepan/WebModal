90ab398113cb42dc4938e1c13ddec0958dafeb3b
Sajeepan <naduvaa@gmail.com>
2014-10-09 14:26:50 +0530

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Web Modal</title>
     </head>
     <body>
         <?php
